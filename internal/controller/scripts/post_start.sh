92897067e7f48358aa321375ee9c48edb2412fc8
Kurt McAlpine <kurt@linux.com>
2025-08-13 12:43:21 +1200

    Don't annouce hostname. This makes java happy.

--- internal/controller/scripts/post_start.sh
+++ internal/controller/scripts/post_start.sh
@@ -2,8 +2,16 @@
 
 set -e
 
-# use hostnames from cluster nodes to re-meet in the case that the ips have been updated
-for host in $(valkey-cli -h 127.0.0.1 -p 6379 -c cluster nodes | awk '!/myself/ { split($2, arr, ","); print arr[2] }'); do
-	ipaddress=$(LC_ALL=C nslookup "$host" 2>/dev/null | sed -nr '/Name/,+1s|Address(es)?: *||p')
-	valkey-cli -h 127.0.0.1 -p 6379 -c cluster meet "$ipaddress" 6379
+# use hostnames from cluster nodes to re-meet in the case that the ips have
+# been updated. The hostname has been set via the nodename field as setting a
+# node hostname to a coreDNS record causes some trouble for clients that don't
+# respect cluster-preferred-endpoint-type when connection from outside kubernetes
+
+# run cluster meet operation 3 times on start up to give time for other pods to start
+for i in {1..3}; do
+	for host in $(awk -F, '/nodename=/ && !/myself/ { split($5, arr, "="); print arr[2] }' nodes.conf); do
+		ipaddress=$(LC_ALL=C nslookup "$host" 2>/dev/null | sed -nr '/Name/,+1s|Address(es)?: *||p')
+		valkey-cli -h 127.0.0.1 -p 6379 -c cluster meet "$ipaddress" 6379
+	done
+	sleep 15
 done
